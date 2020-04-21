940d5a9981c958696ce45cc560edf6ac8566b8d0
tomasmartins <tomasmarianoveloso@gmail.com>
2020-04-21 17:06:15 +0100

    fixing: buttons

---
+++ src/pages/PlayGame.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlayGame = () => {
+  return <h1>Game</h1>;
+};
+
+export default PlayGame;
---
+++ src/pages/TheMinders.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TheMinders = () => {
+  return <h1>The Minders</h1>;
+};
+
+export default TheMinders;
